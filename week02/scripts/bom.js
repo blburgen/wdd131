4a75d639dc501dd81c0baa3452aaa3e6da0bcae7
Brady Burgener <bradylbsee@gmail.com>
2025-10-29 10:06:11 -0600

    adds function to bom

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,11 +2,31 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector("#list");
 
-const li = document.createElement('li');
-const deletButton = document.createElement('button');
+// const li = document.createElement('li');
+// const deleteButton = document.createElement('button');
 
-li.textContent = input.ariaValueMax;
-deletButton.textContent = '❌';
+// li.textContent = input.value;
+// deleteButton.textContent = '❌';
 
-li.append(deletButton);
-list.append(li);
+// li.append(deleteButton);
+// list.append(li);
+
+button.addEventListener("click", function(){
+    if (input.value.trim() !== ''){
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+
+        li.append(deleteButton);
+        list.append(li);
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+        input.value = '';
+    }
+    input.focus();    
+});
+
