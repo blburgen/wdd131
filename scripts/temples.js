cc54d687da83b991cf8683f68131306b681e9fd3
Brady Burgener <bradylbsee@gmail.com>
2025-10-29 12:33:37 -0600

    adds hamburger button

--- scripts/temples.js
+++ scripts/temples.js
@@ -4,3 +4,11 @@ const today = new Date();
 
 year.innerHTML = today.getFullYear();
 document.getElementById("lastModified").innerHTML = "Last Modification: " + document.lastModified;
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector("nav");
+
+hamButton.addEventListener("click", () => {
+	navigation.classList.toggle("open");
+	hamButton.classList.toggle("open");
+});
