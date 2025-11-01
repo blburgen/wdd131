0268bf7e141df4d91e632ba3479237df1994880c
Brady Burgener <bradylbsee@gmail.com>
2025-11-01 15:02:13 -0600

    some updates

--- scripts/places.js
+++ scripts/places.js
@@ -5,12 +5,17 @@ const today = new Date();
 
 const temperature = 50; // temperature in °F
 const windSpeed = 5; // wind speed in mph
+let windChill = "N/A";
+
+if(temperature <= 50 && windSpeed > 3){
+    windChill = calculateWindChill(temperature,windSpeed).toFixed(1);
+}
 
 let weatherInfo = [
     ["Temperature(°F):",temperature],
     ["Conditions:", "Partly Cloudy"],
     ["Wind(mph):",windSpeed],
-    ["Wind Chill(°F):", calculateWindChill(temperature,windSpeed)]
+    ["Wind Chill(°F):", windChill]
 ]
 
 year.innerHTML = today.getFullYear();
@@ -18,11 +23,7 @@ document.getElementById("lastModified").innerHTML = "Last Modification: " + docu
 
 function calculateWindChill(temp, windSpeed){
     // if temperature is less than or equal to 50 °F and wind speed is greater than 3 mph than perform calculation
-    if(temp <= 10 && windSpeed > 4.8){
-        return (35.74 + (0.6215 * temp) - (35.75 * (windSpeed ** 0.16)) + (0.4275 * temp *(windSpeed**0.16)));
-    } else {
-        return "N/A";
-    };
+    return (35.74 + (0.6215 * temp) - (35.75 * (windSpeed ** 0.16)) + (0.4275 * temp *(windSpeed**0.16)));
 }
 
 windchill = calculateWindChill(temperature, windSpeed);
