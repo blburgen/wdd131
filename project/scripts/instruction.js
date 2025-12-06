02dae7e5e7d26ff894166cc8b54dbd1dfb70458e
Brady Burgener <bradylbsee@gmail.com>
2025-12-06 13:34:38 -0700

    adds form

--- project/scripts/instruction.js
+++ project/scripts/instruction.js
@@ -1,71 +1,51 @@
-const products = [
+const trainings = [
   {
-    id: "fc-1888",
-    name: "flux capacitor",
-    averagerating: 4.5
+    id: "oil",
+    name: "Oil painting class"
   },
   {
-    id: "fc-2050",
-    name: "power laces",
-    averagerating: 4.7
+    id: "water",
+    name: "Watercolor painting class"
   },
   {
-    id: "fs-1987",
-    name: "time circuits",
-    averagerating: 3.5
+    id: "out",
+    name: "Outside painting class"
   },
   {
-    id: "ac-2000",
-    name: "low voltage reactor",
-    averagerating: 3.9
+    id: "figure",
+    name: "Figure painting class"
   },
   {
-    id: "jj-1969",
-    name: "warp equalizer",
-    averagerating: 5.0
+    id: "fdraw",
+    name: "Figure drawing class"
   }
 ];
 
-if (product !== null){
-  for (item of products){
+let training = document.getElementById("training");
+
+if (training !== null){
+  for (item of trainings){
     let option = document.createElement("option")
     option.value = item["id"]
     option.textContent = item["name"]
-    product.appendChild(option)
+    training.appendChild(option)
   }
 }
 
-let review = document.querySelector(".review");
 let thankyou = document.querySelector(".thankyou")
 
-if(review !== null){
+if(thankyou !== null){
   let num = 1;
   document.addEventListener("DOMContentLoaded", () =>{
-    const storedCount = localStorage.getItem('formSubmitCount');
+    const storedCount = localStorage.getItem('trainingSubmittedCount');
 
     if (storedCount !== null){
       num += parseInt(storedCount);
-      localStorage.setItem('formSubmitCount',num.toString());
+      localStorage.setItem('trainingSubmittedCount',num.toString());
     } else {
-      localStorage.setItem('formSubmitCount', '1');
+      localStorage.setItem('trainingSubmittedCount', '1');
     }
-    thankyou.innerHTML = `Thank you!  You have submitted ${num} review(s).`;
+    thankyou.innerHTML = `Thank you!  You have submitted ${num} training request(s).`;
   })
-
-  
-
-  for (item of products){
-    let tr = document.createElement("tr");
-    let td0 = document.createElement("td");
-    td0.textContent = item["id"];
-    let td1 = document.createElement("td");
-    td1.textContent = item["name"];
-    let td2 = document.createElement("td");
-    td2.textContent = item["averagerating"];
-    tr.appendChild(td0)
-    tr.appendChild(td1)
-    tr.appendChild(td2)
-    review.appendChild(tr)
-  }
 }
 
